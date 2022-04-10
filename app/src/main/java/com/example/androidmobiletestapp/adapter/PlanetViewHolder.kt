586d0e98ae6ad259bbe5ea7f05ff0f0bb1e0d6d6
Darshok <dfo96@hotmail.com>
2022-04-10 15:54:59 +0200

    Binding view in viewHolder

--- app/src/main/java/com/example/androidmobiletestapp/adapter/PlanetViewHolder.kt
+++ app/src/main/java/com/example/androidmobiletestapp/adapter/PlanetViewHolder.kt
@@ -1,24 +1,28 @@
 package com.example.androidmobiletestapp.adapter
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.androidmobiletestapp.PlanetResponse
 import com.example.androidmobiletestapp.databinding.CardPlanetBinding
 
 
-class PlanetViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class PlanetViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = CardPlanetBinding.bind(view)
-    private val date = binding.tvDate
-    private val copyright = binding.tvCopyright
-    private val title = binding.tvTitle
-    private val planetImage = binding.ivPlanet
 
     fun render(planetModel: PlanetResponse) {
-        date.text = planetModel.date
-        copyright.text = planetModel.copyright
-        title.text = planetModel.title
-        Glide.with(planetImage.context).load(planetModel.imageUrl).into(planetImage)
+        binding.tvDate.text = planetModel.date
+        binding.tvCopyright.text = planetModel.copyright
+        binding.tvTitle.text = planetModel.title
+        Glide.with(binding.ivPlanet.context).load(planetModel.imageUrl).into(binding.ivPlanet)
+        binding.ivPlanet.setOnClickListener {
+            Toast.makeText(
+                binding.ivPlanet.context,
+                planetModel.title,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
