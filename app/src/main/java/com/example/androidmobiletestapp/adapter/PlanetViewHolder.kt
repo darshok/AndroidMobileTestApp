8f54d5b872a47d43401e7838f0dfb7d9d29de809
Darshok <dfo96@hotmail.com>
2022-04-11 16:55:43 +0200

    Added edit planet in the same activity

--- app/src/main/java/com/example/androidmobiletestapp/adapter/PlanetViewHolder.kt
+++ app/src/main/java/com/example/androidmobiletestapp/adapter/PlanetViewHolder.kt
@@ -1,8 +1,11 @@
 package com.example.androidmobiletestapp.adapter
 
+import android.content.Intent
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.androidmobiletestapp.R
+import com.example.androidmobiletestapp.activity.FormPlanetActivity
 import com.example.androidmobiletestapp.data.PlanetResponse
 import com.example.androidmobiletestapp.databinding.CardPlanetBinding
 
@@ -20,5 +23,19 @@ class PlanetViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         binding.ivPlanet.setOnClickListener {
             onClickListener(planetModel)
         }
+
+        binding.btnEditPlanet.setOnClickListener {
+            createFormPlanetActivity(planetModel)
+        }
+    }
+
+    private fun createFormPlanetActivity(planetModel: PlanetResponse) {
+        val intent = Intent(binding.btnEditPlanet.context, FormPlanetActivity::class.java)
+        intent.putExtra("planetCopyright", planetModel.copyright)
+        intent.putExtra("planetDate", planetModel.date)
+        intent.putExtra("planetTitle", planetModel.title)
+        intent.putExtra("planetImageUrl", planetModel.imageUrl)
+        intent.putExtra("topAppBar", binding.btnEditPlanet.context.getString(R.string.edit_planet_app_bar))
+        binding.btnEditPlanet.context.startActivity(intent)
     }
 }
